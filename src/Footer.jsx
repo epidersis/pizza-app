0f88b67040dec87c0d92c78afb86b21d8d38603f
Алексей <epidersis@gmail.com>
2024-04-02 13:29:06 +0900

    updated lint "react/prop-types": "off"

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,11 +1,11 @@
 function Footer() {
-  const hour = new Date().getHours();
-  const openHour = 8;
-  const closeHour = 22;
-  const isOpen = hour >= openHour && hour <= closeHour;
+  // const hour = new Date().getHours();
+  // const openHour = 8;
+  // const closeHour = 22;
+  // const isOpen = hour >= openHour && hour <= closeHour;
   return (
     <footer className="footer">
-      {new Date().toLocaleTimeString()}. We're currently open!
+      {new Date().toLocaleTimeString()}. We&apos;re currently open!
     </footer>
   );
 }
